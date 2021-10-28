d2adf141aa658ff4546039ca835fbfd466933d3c
UdroiuMarius <89607591+UdroiuMarius@users.noreply.github.com>
2021-10-28 09:51:27 +0300

    Add files via upload

---
+++ Pig Game/script.js
@@ -0,0 +1,125 @@
+'use strict';
+
+//Selecting elements
+const player0Element = document.querySelector('.player--0');
+const player1Element = document.querySelector('.player--1');
+const score0Element = document.querySelector('#score--0');
+const score1Element = document.querySelector('#score--1');
+const diceElement = document.querySelector('.dice');
+const current0Elemenent = document.getElementById('current--0');
+const current1Elemenent = document.getElementById('current--1');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+//Starting conditions
+
+let scores, currentScore, activePlayer, playing;
+
+//Starting conditions
+const init = function () {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  score0Element.textContent = 0;
+  score1Element.textContent = 0;
+  current0Elemenent.textContent = 0;
+  current1Elemenent.textContent = 0;
+
+  diceElement.classList.add('hidden');
+  player0Element.classList.remove('player--winner');
+  player1Element.classList.remove('player--winner');
+  player0Element.classList.add('player--active');
+  player1Element.classList.remove('player--active');
+};
+init();
+
+//Actual code
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0Element.classList.toggle('player--active');
+  player1Element.classList.toggle('player--active');
+};
+
+btnRoll.addEventListener('click', function () {
+  if (playing) {
+    //1.Generating a random roll
+    const dice = Math.trunc(Math.random() * 6) + 1;
+    // console.log(dice);
+    //2.Display the random roll
+    diceElement.classList.remove('hidden');
+    diceElement.src = `dice-${dice}.png`;
+    //3.Check if random roll is 1. Yes => switch to player 2
+    //else add the random roll to the player 1 score
+    if (dice != 1) {
+      currentScore += dice;
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
+    } else {
+      // switch to next player
+      switchPlayer();
+    }
+  }
+});
+
+btnHold.addEventListener('click', function () {
+  if (playing) {
+    //1.add current score to player score
+    scores[activePlayer] += currentScore;
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer];
+    //2.Check if the score is >=100
+    if (scores[activePlayer] >= 100) {
+      //Finish the game
+      playing = false;
+      diceElement.classList.add('hidden');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--winner');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--active');
+    } else {
+      //3.Switch to the next player
+      switchPlayer();
+    }
+  }
+});
+
+btnNew.addEventListener('click', init);
+
+/*
+function toCamelCase(str) {
+  let newString = '';
+
+  if (!str) {
+    newString = '';
+  } else {
+    newString += str[0];
+  }
+
+  if (str) {
+    for (let i = 1; i < str.length; i++)
+      if (str[i] !== '-' && str[i] !== '_') {
+        if (str[i - 1] === '-' || str[i - 1] === '_')
+          newString += str[i].toUpperCase();
+        else newString += str[i];
+      } else {
+        // newString += str[i + 1].toUpperCase();
+        continue;
+      }
+  } else newString = '';
+
+  return newString;
+}
+console.log(toCamelCase('the_stealth_warrior'));
+*/
+
+function toCamelCase1(str) {
+  return str.replace(/[-_](.)/g, (_, c) => c.toUpperCase());
+}
+console.log(toCamelCase1('the_stealth_warrior'));
